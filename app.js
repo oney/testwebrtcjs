0fa1082f02073696c77f3bee29c3bf4958e1abdd
root <root@ip-172-31-1-143.ap-southeast-1.compute.internal>
2015-09-23 17:45:02 +0000

    temp

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var roomList = {};
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-http.listen(3001, function(){
+http.listen(80, function(){
   console.log('listening on *:3001');
 });
 
